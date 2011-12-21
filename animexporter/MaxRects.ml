f0894959ccff446ccfd52e355ba7b7eb09c49820
pushkin <pushkin666@inbox.ru>
2011-12-21 18:54:28 +0300

    Fix convert script

--- animexporter/MaxRects.ml
+++ animexporter/MaxRects.ml
@@ -12,7 +12,7 @@ value rec layout_page init_rects rects w h =
     ]
   in
   let () = Printf.printf "Layout page x:%d y:%d w:%d h:%d \n%!" 0 0 w h in
-  let (placed, empty, rest) = TextureLayout.maxrects rects [] init_rects [] in
+  let (placed, empty, rest) = TextureLayout.maxrects ~not_rotate:True rects [] init_rects [] in
   match rest with 
   [ [] -> (w, h, placed, empty,rest) (* разместили все *)
   | _  -> (!max_size, !max_size, [], init_rects, rest)
@@ -20,28 +20,25 @@ value rec layout_page init_rects rects w h =
 
 (* размещаем на нескольких страницах *)
 value rec layout_multipage rects pages = 
-  let (need_new_page, pages)  = 
-    List.fold_left begin fun (need, res) ((w,h,placed', empty') as page) ->
+  let (need_new_page,_,i,placed, pages)  = 
+    List.fold_left begin fun (need,i,res_i,res_placed, res) ((w,h,placed', empty') as page) ->
       match need with
       [ True -> 
           let (w, h, placed, empty, rest) = layout_page empty' rects w h in
           match rest with
-          [ [] ->  (False, [(w,h, placed' @ placed , empty) :: res])
-          | _ -> (need, [page :: res ]) 
+          [ [] ->  (False, i+1, i, placed, [(w,h, placed' @ placed , empty) :: res])
+          | _ -> (need, i+1, res_i, res_placed, [page :: res ]) 
           ]
-      | _ -> (need, [ page :: res ])
+      | _ -> (need, i+1, res_i,res_placed, [ page :: res ])
       ]
-    end (True, []) pages
+    end (True,0,0,[], []) pages
   in
   let pages = List.rev pages in
     match need_new_page with
     [ True -> 
         let (w, h, placed, empty,rects) = layout_page [] rects !min_size !min_size in
-        let () = Printf.printf "Placed length %d \n%!" (List.length placed) in
-        let () = Printf.printf "Rects length %d \n%!" (List.length rects) in
-        let () = Printf.printf "Empty length %d \n%!" (List.length empty) in
-        pages @ [ (w,h,placed,empty) ]
-    | _ -> pages
+        (List.length pages, placed, pages @ [ (w,h,placed,empty) ])
+    | _ -> (i, placed, pages)
     ];
 
 value layout  ?(pages=[])  rects =
--- animexporter/convert.ml
+++ animexporter/convert.ml
@@ -6,8 +6,8 @@ value (///) = Filename.concat;
 type layer =
   {
     imgId   : int;
-    x       : int;
-    y       : int;
+    lx       : int;
+    ly       : int;
     alpha   : int;
     flip    : bool;
   };
@@ -32,12 +32,50 @@ value write_utf out str=
   );
 
 value inp_dir = "../../mobile-farm/Resources/library";
-(*value inp_dir = "input"; *)
+(*
+value inp_dir = "input"; 
+ *)
 value outdir = "output";
 
 value get_images dir images  =
   let texInfo = inp_dir /// dir /// texInfo in
-  let () = Printf.printf "try open %s \n%!" texInfo in
+  let frameInfo =BatFile.open_in (inp_dir /// dir /// "frames.dat") in 
+  let rec getLayers cnt layers =
+    match cnt with
+    [ 0 -> layers 
+    | _ ->
+        (
+          ignore(BatIO.read_byte frameInfo);
+          let imgId = BatIO.read_i32 frameInfo in
+          let lx = BatIO.read_ui16 frameInfo in
+          let ly = BatIO.read_ui16 frameInfo in
+          let alpha = BatIO.read_byte frameInfo in
+          let flip = 
+            match BatIO.read_byte frameInfo with
+            [ 0 -> False 
+            | _ -> True
+            ]
+          in
+            (
+              getLayers (cnt-1) [ {imgId; lx; ly; alpha; flip} :: layers]
+            )
+        )
+    ]
+  in
+  let rec getFrames cnt frames = 
+    match cnt with
+    [ 0 -> frames 
+    | _ ->
+        let x = BatIO.read_ui16 frameInfo in
+        let y = BatIO.read_ui16 frameInfo in
+        let iconX = BatIO.read_ui16 frameInfo in
+        let iconY = BatIO.read_ui16 frameInfo in
+        let layers = getLayers (BatIO.read_byte frameInfo) [] in
+        getFrames (cnt - 1) [ {x;y; iconX;iconY; layers} :: frames ]
+
+    ]
+  in
+  let frames = getFrames (BatIO.read_i32 frameInfo) [] in
   let texInfo = BatFile.open_in texInfo in 
   let countTex = BatIO.read_ui16 texInfo in
   let () = Printf.printf "count textures : %d\n%!" countTex in
@@ -49,7 +87,7 @@ value get_images dir images  =
         let sy = BatIO.read_ui16 texInfo in
         let iw = BatIO.read_ui16 texInfo in
         let ih = BatIO.read_ui16 texInfo in
-        imagesByTexture img (countImage - 1) (id +1) (s + iw * ih) [ (id, Images.sub img sx sy iw ih) :: images ]
+        imagesByTexture img (countImage - 1) (id +1) (s + iw * ih) [ ((id,dir), Images.sub img sx sy iw ih) :: images ]
     ]
   in
   let rec readTexture countTex (id,s,images) =
@@ -69,43 +107,35 @@ value get_images dir images  =
       res
     );
 
-value convert dir textures images =
+value convert dir idTex imgs =
+  let dir = outdir /// dir in
     (
-      Printf.printf "find rects for %s\n%!" dir;
-      let textures = MaxRects.layout ~pages:textures images in
-      let dir = outdir /// dir in
+      Printf.printf "Convert rects for %s\n%!" dir;
+      match Sys.file_exists dir with
+      [ False -> Unix.mkdir dir 0o755
+      | _ -> ()
+      ];
+      let newTexInfo = dir /// "texInfo.dat" in
+      let newTexInfo = BatFile.open_out ~mode:[`create ] newTexInfo in
         (
-          match Sys.file_exists dir with
-          [ False -> Unix.mkdir dir 0o755
-          | _ -> ()
-          ];
-          let newTexInfo = dir /// "textInfo.dat" in
-          let newTexInfo = BatFile.open_out ~mode:[`create ] newTexInfo in
+          BatIO.write_ui16 newTexInfo 1;
+          write_utf newTexInfo (".." /// (string_of_int idTex) ^ ".png");
+          BatIO.write_ui16 newTexInfo (List.length imgs);
+          BatList.iteri begin fun i (urlId,(sx,sy,isRotate,img)) ->
+          (
+            let (iw,ih) = Images.size img in
             (
-              BatIO.write_ui16 newTexInfo (List.length textures);
-              BatList.iteri begin fun cnt (w,h,imgs,_) ->
-                let () = write_utf newTexInfo ((string_of_int cnt) ^ ".png") in
-                let () = BatIO.write_ui16 newTexInfo (List.length imgs) in
-                  (
-                    BatList.iteri begin fun i (urlId,(sx,sy,isRotate,img)) ->
-                    (
-                      let (iw,ih) = Images.size img in
-                      (
-                        BatIO.write_ui16 newTexInfo sx;
-                        BatIO.write_ui16 newTexInfo sy;
-                        BatIO.write_ui16 newTexInfo iw;
-                        BatIO.write_ui16 newTexInfo ih;
-                        BatIO.write_byte newTexInfo (match isRotate with [ True -> 1 | _ -> 0]);
-                      );
-                    )
-                    end imgs;
-                  )
-              end textures;
-              BatIO.close_out newTexInfo;
+              BatIO.write_ui16 newTexInfo sx;
+              BatIO.write_ui16 newTexInfo sy;
+              BatIO.write_ui16 newTexInfo iw;
+              BatIO.write_ui16 newTexInfo ih;
+           (*   BatIO.write_byte newTexInfo (match isRotate with [ True -> 1 | _ -> 0]); *)
             );
-            textures
-          );
-    );
+          )
+          end imgs;
+          BatIO.close_out newTexInfo;
+        );
+      );
     
 value run () =
   let images = 
@@ -119,12 +149,19 @@ value run () =
   in
 (*  let images = [(0,"pizda", List.fold_left (fun res (_,_,img) ->  res @ img) [] images)] in *)
   let images = List.fast_sort (fun (s1,_,_) (s2,_,_) -> compare s2 s1) images in  
-  let textures = List.fold_left (fun textures  (_,path,images) -> convert path textures images) [] images in 
+  let textures = List.fold_left begin fun textures  (_,path,images) -> 
+    let (texId, placed, textures) = MaxRects.layout ~pages:textures images in
+      (
+        convert path texId placed;
+        textures;
+      )
+  end [] images in 
   BatList.iteri begin fun cnt (w,h,imgs,_) ->
     let rgb = Rgba32.make w h {Color.color={Color.r=0;g=0;b=0}; alpha=0;} in
     let new_img = Images.Rgba32 rgb in
       (
-        BatList.iteri begin fun i (urlId,(sx,sy,isRotate,img)) ->
+        Printf.printf "Save %d.png \n%!" cnt;
+        BatList.iteri begin fun i (_,(sx,sy,isRotate,img)) ->
           let (iw,ih) = Images.size img in
             try
               Images.blit img 0 0 new_img sx sy iw ih;
@@ -140,7 +177,7 @@ value run () =
                     raise Exit;
                   )
               ]
-        end (List.rev imgs);
+        end imgs;
         Images.save (outdir /// ((string_of_int cnt) ^ ".png")) (Some Images.Png) [] new_img;
       )
   end textures;
