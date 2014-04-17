f9431139a2e71b8a6900118bfb7928b1132b7e25
nazarov.andrey <nazarov.andrey@me.com>
2014-04-17 12:53:48 +0400

    added option to print points as json array

--- contour-generator/tools/cntrview.ml
+++ contour-generator/tools/cntrview.ml
@@ -1,12 +1,14 @@
 value alphaThreshold = ref None;
 value lineThreshold = ref None;
 value inp = ref None;
+value out = ref None;
 
 Arg.parse
 	[
-  	("-at", Arg.Int (fun t -> alphaThreshold.val := Some t), "\talpha threshold (default is 0)");
-  	("-lt", Arg.Float (fun t -> lineThreshold.val := Some t), "\tline threshold (default is 5)");
-	] (fun fname -> inp.val := Some fname) "Simple contour viewer.\nUsage: cntrview [-at number] [-lt number] filename.";
+  	("-at", Arg.Int (fun t -> alphaThreshold.val := Some t), "\tAlpha threshold (default is 0).");
+  	("-lt", Arg.Float (fun t -> lineThreshold.val := Some t), "\tLine threshold (default is 5).");
+  	("-o", Arg.String (fun o -> out.val := Some o), "\tSpecify output file for contour points.");
+	] (fun fname -> inp.val := Some fname) "Simple contour viewer.\nUsage: cntrview [-at number] [-lt number] [-o filename] filename.";
 
 match !inp with
 [ Some inp ->
@@ -37,10 +39,21 @@ match !inp with
 
 				let alphaThreshold = !alphaThreshold in
 				let lineThreshold = !lineThreshold in
-				let contour = Array.of_list (List.map (fun (x, y) -> (x + 25, h - y + 25)) (Contour.gen ?alphaThreshold ?lineThreshold img)) in
+				let contour = Contour.gen ?alphaThreshold ?lineThreshold img in
+				let contourAr = Array.of_list (List.map (fun (x, y) -> (x + 25, h - y + 25)) contour) in
 					(
-						Printf.printf "contour generated, points number: %d\n%!" (Array.length contour);
-						Graphics.draw_poly contour;
+						match !out with
+						[ Some out ->
+							let out = open_out out in
+								(
+									output_string out (Printf.sprintf "[ %s ]" (String.concat "," (List.map (fun (x, y) -> Printf.sprintf "%d,%d" x y) contour)));
+									close_out out;
+								)
+						| _ -> ()
+						];
+
+						Printf.printf "contour generated, points number: %d\n%!" (Array.length contourAr);
+						Graphics.draw_poly contourAr;
 					);
 
 	      ignore(Graphics.wait_next_event [Graphics.Key_pressed]);
