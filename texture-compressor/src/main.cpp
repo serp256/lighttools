86e4066c7ba2c52d85b106661d4a19e743ad0420
nazarov.andrey <nazarov.andrey@me.com>
2014-06-18 18:53:35 +0400

    texture compressor: 4444 format support

--- texture-compressor/src/main.cpp
+++ texture-compressor/src/main.cpp
@@ -1,6 +1,7 @@
 #include <getopt.h>
 #include "QCompressLib.h"
 #include <unistd.h>
+#include <sys/stat.h>
 
 #define ERR_IF(cond, fmt, args...) if (cond) { printf("\n"); printf(fmt, ## args); printf("\n"); exit(1); }
 #define PRINT(fmt, args...) if (!silent) { printf(fmt, ## args); fflush(stdout); }
@@ -12,9 +13,11 @@
 #define ETC2_EXT "etc2"
 #define KTX_EXT "ktx"
 
-int pvr = 0, atc = 0, dxt = 0, etc_fast = 0, etc_slow = 0, etc2_fast = 0, etc2_slow = 0, no_alpha = 0, silent = 0;
+#define COMPRESSED_EXT "cmprs"
 
-char* change_ext(char *inp, char *new_ext) {
+int pvr = 0, atc = 0, dxt = 0, etc_fast = 0, etc_slow = 0, etc2_fast = 0, etc2_slow = 0, ffff = 0, no_alpha = 0, silent = 0;
+
+char *change_ext(char *inp, char *new_ext) {
 	char *cur_ext = strrchr(inp, '.');
 	int inp_len = cur_ext ? inp_len = strlen(inp) - strlen(cur_ext) : strlen(inp);
 	char *ret = (char*)malloc(inp_len + strlen(new_ext) + 2);
@@ -22,6 +25,44 @@ char* change_ext(char *inp, char *new_ext) {
 	memcpy(ret, inp, inp_len);
 	strcpy(ret + inp_len + 1, new_ext);
 	ret[inp_len] = '.';
+
+	return ret;
+}
+
+char *insert_dirname(char *inp, char *dirname, char make_dir, char *ext) {
+	char *fname = strrchr(inp, '/');
+	char *with_dir = NULL;
+	size_t dirname_len = strlen(dirname);
+
+	if (!fname) {
+		size_t inp_len = strlen(inp);
+		with_dir = (char*)malloc(dirname_len + 1 + inp_len + 1);
+
+		strcpy(with_dir, dirname);
+		mkdir(dirname, 0775);
+			 *(with_dir + dirname_len) = '/';
+		strcpy(with_dir + dirname_len + 1, inp);
+	} else {
+		fname++;
+		size_t fname_len = strlen(fname);
+		size_t basedir_len = strlen(inp) - fname_len;
+		
+		with_dir = (char*)malloc(basedir_len + dirname_len + fname_len + 2);
+
+		memcpy(with_dir, inp, basedir_len);
+		strcpy(with_dir + basedir_len, dirname);
+		mkdir(with_dir, 0775);
+			 *(with_dir + basedir_len + dirname_len) = '/';
+		strcpy(with_dir + basedir_len + dirname_len + 1, fname);
+	}
+
+	char *ret;
+	if (ext) {
+		ret = change_ext(with_dir, ext);
+		free(with_dir);
+	} else {
+		ret = with_dir;
+	}
 
 	return ret;
 }
@@ -43,12 +84,24 @@ void compress_using_qonvert(char *inp, char *out, unsigned int format) {
 	FreeTexture(mips[0]);
 }
 
+void compress_using_pvrtextool(char *inp, char *out, char *format) {
+	char *pvr_out = change_ext(out, PVR_EXT);
+	char *fmt = "PVRTexTool -yflip0 -f%s -premultalpha -pvrtcbest -i %s -o %s > /dev/null 2>&1";
+	char *cmd = (char*)malloc(strlen(fmt) - 6 + strlen(inp) + strlen(pvr_out) + 1);
+	sprintf(cmd, fmt, format, inp, pvr_out);
+	// printf("cmd %s\n", cmd);
+	ERR_IF(system(cmd), "error when running pvr tool on %s", inp);
+	ERR_IF(rename(pvr_out, out), "error when renaming '%s' to '%s'", pvr_out, out);
+	free(cmd);
+	free(pvr_out);
+}
+
 void compress(char *inp) {
 	char *out;
 
 	if (atc) {
 		PRINT("\tmaking atc... ");
-		out = change_ext(inp, AST_EXT);
+		out = insert_dirname(inp, AST_EXT, 1, COMPRESSED_EXT);
 		compress_using_qonvert(inp, out, no_alpha ? Q_FORMAT_ATC_RGB : Q_FORMAT_ATC_RGBA_EXPLICIT_ALPHA);
 		free(out);
 		PRINT("done\n");
@@ -56,21 +109,25 @@ void compress(char *inp) {
 
 	if (dxt) {
 		PRINT("\tmaking dxt... ");
-		out = change_ext(inp, DXT_EXT);
+		out = insert_dirname(inp, DXT_EXT, 1, COMPRESSED_EXT);
 		compress_using_qonvert(inp, out, no_alpha ? Q_FORMAT_S3TC_DXT1_RGB : Q_FORMAT_S3TC_DXT5_RGBA);
+		free(out);
+		PRINT("done\n");
+	}
+
+	if (ffff) {
+		PRINT("\tmaking 4444... ");
+		out = change_ext(inp, COMPRESSED_EXT);
+		compress_using_pvrtextool(inp, out, "OGL4444");
 		free(out);
 		PRINT("done\n");
 	}
 
 	if (pvr) {
 		PRINT("\tmaking pvr... ");
-		out = change_ext(inp, PVR_EXT);
-		char *fmt = "PVRTexTool -yflip0 -fOGLPVRTC4 -premultalpha -pvrtcbest -i %s -o %s 1>&- 2>&-";
-		char *cmd = (char*)malloc(strlen(fmt) - 4 + strlen(inp) + strlen(out) + 1);
-		sprintf(cmd, fmt, inp, out);
-		ERR_IF(system(cmd), "error when running pvr tool on %s", inp);
+		out = insert_dirname(inp, PVR_EXT, 1, COMPRESSED_EXT);
+		compress_using_pvrtextool(inp, out, "OGLPVRTC4");
 		free(out);
-		free(cmd);
 		PRINT("done\n");
 	}
 
@@ -99,7 +156,7 @@ void compress(char *inp) {
 
 		char *speed = etc_slow ? (char*)"slow" : (char*)"fast";
 		char *fmt = "etcpack %s %s -s %s -c etc1 -as -ktx 1>&- 2>&-";
-		char *cmd = (char*)malloc(strlen(fmt) - 4 + strlen(inp) + tmpdir_len + 1);
+		char *cmd = (char*)malloc(strlen(fmt) - 6 + strlen(inp) + tmpdir_len + 1);
 		sprintf(cmd, fmt, inp, tmpdir, speed);
 		ERR_IF(system(cmd), "error when running etcpack tool on %s", inp);
 
@@ -111,7 +168,7 @@ void compress(char *inp) {
 	char *ext = strrchr(src, '.'); \
 	size_t ext_len = strlen(ext); \
 	memcpy(res, src, src_len); \
-	strcpy(res + src_len - ext_len, "_alpha"); \
+	memcpy(res + src_len - ext_len, "_alpha", 6); \
 	strcpy(res + src_len - ext_len + 6, ext); \
 };
 
@@ -119,7 +176,7 @@ void compress(char *inp) {
 		char *ktx_alpha;
 		ALPHA_FNAME(ktx, ktx_alpha);
 
-		out = change_ext(inp, ETC_EXT);
+		out = insert_dirname(inp, ETC_EXT, 1, COMPRESSED_EXT);
 		RESAVE(ktx, out);
 		
 		char *out_alpha;
@@ -149,14 +206,14 @@ void compress(char *inp) {
 		char *speed = etc_slow ? (char*)"slow" : (char*)"fast";
 		char *pxl_fmt = no_alpha ? (char*)"RGB" : (char*)"RGBA";
 		char *fmt = "etcpack %s %s -s %s -f %s -c etc2 -ktx 1>&- 2>&-";
-		char *cmd = (char*)malloc(strlen(fmt) - 4 + strlen(inp) + tmpdir_len + 1);
+		char *cmd = (char*)malloc(strlen(fmt) - 8 + strlen(inp) + tmpdir_len + 1);
 		sprintf(cmd, fmt, inp, tmpdir, speed, pxl_fmt);
 		ERR_IF(system(cmd), "error when running etcpack tool on %s", inp);
 
 		FNAMES;
 
 		char *ktx = change_ext(tmp_fname, KTX_EXT);
-		out = change_ext(inp, ETC2_EXT);
+		out = insert_dirname(inp, ETC2_EXT, 1, COMPRESSED_EXT);
 		RESAVE(ktx, out);
 		unlink(ktx);
 
@@ -182,6 +239,7 @@ int main(int argc, char **argv) {
 		{"etc-slow", no_argument, &etc_slow, 1},
 		{"etc2-slow", no_argument, &etc2_slow, 1},
 		{"no-alpha", no_argument, &no_alpha, 1},
+		{"4444", no_argument, &ffff, 1},
 		{"silent", no_argument, &silent, 1},
 		{0, 0, 0, 0}
 	};
