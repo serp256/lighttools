8f08f84c325920cbcc989123b59ef4811e7a4a57
pushkin <pushkin666@inbox.ru>
2014-11-17 14:04:05 +0400

    fix rule file param

--- lsync2/lsync2.ml
+++ lsync2/lsync2.ml
@@ -2,14 +2,38 @@ value binFname = "lsync2run";
 value binPath = Filename.concat "." binFname;
 value rulesFile = ref "lsync2_rules.ml";
 
+value args  = 
+  let cmd_params  = ref "" in
+  let skip_i = ref 0 in
+  let argv = Sys.argv in
+  (
+    Array.iteri (fun i param -> 
+      match i = !skip_i with
+      [ True -> ()
+      | _ -> 
+          match param with
+          [ "-rule-file" -> 
+              (
+                rulesFile.val := argv.(i+1);
+                skip_i.val := i + 1;
+              )
+          | _ -> cmd_params.val := !cmd_params ^ param ^ " " 
+          ]
+      ]
+    ) argv;
+    !cmd_params;
+  );
+
+(*
 Arg.parse 
   [
     ("-rule-file",  Arg.Set_string rulesFile, "rules name file bu default lsync2_rules.ml");
   ]
   (fun _ -> ())
   "";
+  *)
 
-  value rulesPath = Filename.concat "." !rulesFile;
+value rulesPath = Filename.concat "." !rulesFile;
 
 value compile () =
   if Sys.file_exists rulesPath
@@ -34,7 +58,9 @@ then
     else ()
 else compile ();
 
+(*
 let args = String.concat " " (List.tl (Array.to_list Sys.argv)) in
+*)
 let cmd = binPath ^ " " ^ args in
   if Sys.command cmd <> 0
   then failwith ("Error when running '" ^ binPath ^ "'")
